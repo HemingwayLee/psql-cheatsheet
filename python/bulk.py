75d4a72d99336d7670f631f62e49e78848ac1c33
LeeHemingway <coherence0815@gmail.com>
2020-11-14 16:22:50 +0900

    add another execute_values example

--- python/bulk.py
+++ python/bulk.py
@@ -1,5 +1,7 @@
+import traceback
 import psycopg2
 from psycopg2.extras import execute_values
+from psycopg2 import sql
 
 try:
     conn = psycopg2.connect("dbname='test' host='127.0.0.1'")
@@ -10,9 +12,14 @@ cur = conn.cursor()
 try:
     arr = [("James", 26), ("Kristof", 25)]
     execute_values(cur, "INSERT INTO aaa (name, age) VALUES %s", arr)
+    conn.commit()
 
+    tablename = "aaa"
+    queryText = "INSERT INTO {table} (name, age) VALUES %s"
+    query = sql.SQL(queryText).format(table=sql.Identifier(tablename))
+    execute_values(cur, query.as_string(cur), arr)
     conn.commit()
   
 except:
-    print("unexpected error")
+    print(traceback.format_exc())
 
